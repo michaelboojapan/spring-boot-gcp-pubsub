7481c5fc0d871884dbbe14a5be402e268673aa9d
Bao, Zhao | Boo | ECFD <zhaorigetu.bao@rakuten.com>
2021-05-08 14:26:11 +0900

    test

---
+++ complete/src/main/java/hello/WebAppController.java
@@ -0,0 +1,27 @@
+package hello;
+
+import hello.PubSubApplication.PubsubOutboundGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class WebAppController {
+
+  // tag::autowireGateway[]
+  @Autowired
+  private PubsubOutboundGateway messagingGateway;
+  @Autowired
+  private PubSubInboundChannelAdapter getMessage;
+  // end::autowireGateway[]
+
+  @PostMapping("/publishMessage")
+  public RedirectView publishMessage(@RequestParam("message") String message) {
+	messagingGateway.sendToPubsub(message);
+	return new RedirectView("/");
+  }
+
+}
